024d526dd6affa2080dc012bb641cc453589ec58
Wave-Shot <vsh.2509@gmail.com>
2026-04-27 20:42:00 +0530

    [Srihari]: UC3 Validate Single Word Constraint completed

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -4,13 +4,19 @@ public class LexicalTwistPuzzle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter first word:");
         String word1 = sc.nextLine();
-
-        System.out.println("Enter second word:");
         String word2 = sc.nextLine();
 
-        System.out.println("Word1: " + word1);
-        System.out.println("Word2: " + word2);
+        if (word1.contains(" ")) {
+            System.out.println(word1 + " is an invalid word");
+            return;
+        }
+
+        if (word2.contains(" ")) {
+            System.out.println(word2 + " is an invalid word");
+            return;
+        }
+
+        System.out.println("Valid Inputs");
     }
 }
