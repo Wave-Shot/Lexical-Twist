375b7fdc4a813cd1b37a209e647ecda3e803e5ea
Wave-Shot <vsh.2509@gmail.com>
2026-04-27 20:43:19 +0530

    [Srihari]: UC7 Rule Based Output completed

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -7,26 +7,43 @@ public class LexicalTwistPuzzle {
         String word1 = sc.nextLine();
         String word2 = sc.nextLine();
 
-        String reversed = new StringBuilder(word1).reverse().toString();
+        String combined = (word1 + word2).toUpperCase();
 
-        if (!reversed.equalsIgnoreCase(word2)) {
-            String combined = (word1 + word2).toUpperCase();
+        int vowels = 0;
+        int consonants = 0;
 
-            int vowels = 0;
-            int consonants = 0;
+        for (int i = 0; i < combined.length(); i++) {
+            char c = combined.charAt(i);
 
+            if ("AEIOU".indexOf(c) != -1) {
+                vowels++;
+            } else if (Character.isLetter(c)) {
+                consonants++;
+            }
+        }
+
+        String result = "";
+
+        if (vowels > consonants) {
             for (int i = 0; i < combined.length(); i++) {
                 char c = combined.charAt(i);
-
-                if ("AEIOU".indexOf(c) != -1) {
-                    vowels++;
-                } else if (Character.isLetter(c)) {
-                    consonants++;
+                if ("AEIOU".indexOf(c) != -1 && result.indexOf(c) == -1) {
+                    result += c;
                 }
+                if (result.length() == 2) break;
             }
-
-            System.out.println("Vowels: " + vowels);
-            System.out.println("Consonants: " + consonants);
+            System.out.println(result);
+        } else if (consonants > vowels) {
+            for (int i = 0; i < combined.length(); i++) {
+                char c = combined.charAt(i);
+                if ("AEIOU".indexOf(c) == -1 && Character.isLetter(c) && result.indexOf(c) == -1) {
+                    result += c;
+                }
+                if (result.length() == 2) break;
+            }
+            System.out.println(result);
+        } else {
+            System.out.println("Vowels and consonants are equal");
         }
     }
 }
