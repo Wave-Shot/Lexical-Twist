a1db18d3702ed965ae95c0546780eefa6c0faae7
Wave-Shot <vsh.2509@gmail.com>
2026-04-27 20:41:46 +0530

    [Srihari]: UC2 Accept Word Inputs completed

--- src/LexicalTwistPuzzle.java
+++ src/LexicalTwistPuzzle.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class LexicalTwistPuzzle {
     public static void main(String[] args) {
-        System.out.println("Lexical Twist Puzzle");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first word:");
+        String word1 = sc.nextLine();
+
+        System.out.println("Enter second word:");
+        String word2 = sc.nextLine();
+
+        System.out.println("Word1: " + word1);
+        System.out.println("Word2: " + word2);
     }
 }
