b5351982a80d3a4766a1abbdf3b900cf6feb0863
Wave-Shot <vsh.2509@gmail.com>
2026-04-27 20:47:23 +0530

    [Srihari]: Cleaned main branch, kept only README

--- src/WordValidator.java
+++
@@ -1,9 +0,0 @@
-public class WordValidator {
-    public static boolean isValid(String word) {
-        if (word.contains(" ")) {
-            System.out.println(word + " is an invalid word");
-            return false;
-        }
-        return true;
-    }
-}
